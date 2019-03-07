3f04687a214210edaef74e147f6965d5152f0ae3
aprild4300 <48287716+aprild4300@users.noreply.github.com>
2019-03-07 14:19:43 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -26,12 +26,12 @@
                   }).then(function(response){
               console.log(response);
            var enc = response[0];
-           
+          
           console.log (enc);
           for (var i = 0, len = enc.identifier.length; i < len; i++) {
             if (enc.identifier[i].type !== null && enc.identifier[i].type.text !== null && enc.identifier[i].type.text =='FIN NBR')
             {[fin=enc.identifier[i].value]};
-            if (enc.identifier[i].location !== null && enc.identifier[i].location.display !== null)
+            if (enc.identifier[i].location !== null && location[""0""].location.display !== null)
             {[location=enc.identifier[i].location.display]};
             if (enc.identifier[i].reason !== null && enc.identifier[i].reason.text !== null)
             {[reason=enc.identifier[i].reason.text]};
